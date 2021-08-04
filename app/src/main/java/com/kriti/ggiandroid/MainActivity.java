d0caab026670999da9da4864c881e244aa152e37
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-08-05 00:17:22 +0530

    Initial commit

---
+++ app/src/main/java/com/kriti/ggiandroid/MainActivity.java
@@ -0,0 +1,40 @@
+package com.kriti.ggiandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/*
+1. Register the components by their id
+2. Action perform
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText; TextView textView; Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        //Register the components
+        editText = findViewById(R.id.userName);
+        textView = findViewById(R.id.textViewName);
+        button = findViewById(R.id.buttonName);
+
+        //What will happen when the button is clicked
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editText.getText().toString();
+                textView.setText(name);
+            }
+        });
+    }
+}
